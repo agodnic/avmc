e37bad452054b923cf9d66024f8069ec7a30eb52
Agustin Godnic <agustingodnic@gmail.com>
2026-05-21 18:46:24 -0300

    Experiment

--- parser/nodes_test.go
+++ parser/nodes_test.go
@@ -8,15 +8,32 @@ import (
 )
 
 const code = `
-func main
+func main () {
+	return 0
+}
 `
 
 type CompulationUnit struct {
+	// FIXME use an union to have different types of declarations
 	FuncDeclarations []*FuncDeclaration `@@*`
 }
 
 type FuncDeclaration struct {
-	Name string `"func" @Ident`
+	Func   string `"func"`
+	Name   string `@Ident`
+	LParen string `"("`
+	RParen string `")"`
+	LBrace string `"{"`
+
+	// TODO use an union to have different types of statements
+	Stmts []*Stmt `@@*`
+
+	RBrace string `"}"`
+}
+
+type Stmt struct {
+	Return string `"return"`
+	UInt   uint64 `@Int`
 }
 
 func TestExperiment(t *testing.T) {
@@ -30,6 +47,12 @@ func TestExperiment(t *testing.T) {
 	compilationUnit, err := parser.ParseString("", code)
 	assert.NoError(t, err)
 
-	assert.Equal(t, 1, len(compilationUnit.FuncDeclarations))
-	assert.Equal(t, "main", compilationUnit.FuncDeclarations[0].Name)
+	assert.Len(t, compilationUnit.FuncDeclarations, 1)
+
+	decl := compilationUnit.FuncDeclarations[0]
+	assert.Equal(t, "main", decl.Name)
+	assert.Len(t, decl.Stmts, 1)
+
+	stmt := decl.Stmts[0]
+	assert.Equal(t, uint64(0), stmt.UInt)
 }
