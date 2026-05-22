95a0764ea8cb7a04e2fe76f20b6079f29fe885e1
Agustin Godnic <agustingodnic@gmail.com>
2026-05-21 21:10:44 -0300

    Refactor

--- parser/nodes.go
+++ parser/nodes.go
@@ -1,40 +1,45 @@
 package parser
 
-type CompilationUnit struct {
-	// FIXME use an union to have different types of declarations
-	FuncDeclarations []FuncDeclaration `@@*`
-}
+type (
+	CompilationUnit struct {
+		// FIXME use an union to have different types of declarations
+		FuncDeclarations []FuncDeclaration `@@*`
+	}
 
-type FuncDeclaration struct {
-	Func               string              `"func"`
-	Name               string              `@Ident`
-	LParen             string              `"("`
-	FunctionParameters []FunctionParameter `@@! ("," @@)*`
-	RParen             string              `")"`
-	ReturnType         string              `@Ident`
-	LBrace             string              `"{"`
-	Stmts              []Stmt              `@@+`
-	RBrace             string              `"}"`
-}
+	FuncDeclaration struct {
+		Func               string              `"func"`
+		Name               string              `@Ident`
+		LParen             string              `"("`
+		FunctionParameters []FunctionParameter `@@! ("," @@)*`
+		RParen             string              `")"`
+		ReturnType         string              `@Ident`
+		LBrace             string              `"{"`
+		Stmts              []Stmt              `@@+`
+		RBrace             string              `"}"`
+	}
 
-type FunctionParameter struct {
-	Ident string `@Ident`
-	Type  string `@Ident`
-}
+	FunctionParameter struct {
+		Ident string `@Ident`
+		Type  string `@Ident`
+	}
 
-type ReturnStmt struct {
-	Return string `"return"`
-	UInt   uint64 `@Int` //TODO should be an expr node
-}
+	ReturnStmt struct {
+		Return string `"return"`
+		UInt   uint64 `@Int` //TODO should be an expr node
+	}
 
-type VariableDeclarationStmt struct {
-	Var   string `"var"`
-	Ident string `@Ident`
-	Eq    string `"="`
-	Expr  string `@Int` //FIXME should be an expr node
-}
+	VariableDeclarationStmt struct {
+		Var   string `"var"`
+		Ident string `@Ident`
+		Eq    string `"="`
+		Expr  string `@Int` //FIXME should be an expr node
+	}
+)
 
-type Stmt interface{ stmtTag() }
+type (
+	Stmt interface{ stmtTag() }
+	Expr interface{ exprTag() }
+)
 
 func (rs ReturnStmt) stmtTag()              {}
 func (rs VariableDeclarationStmt) stmtTag() {}
