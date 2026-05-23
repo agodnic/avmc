2847aafa9f0bd6a7f00cfb4ef06f7b63dbfab9cc
Agustin Godnic <agustingodnic@gmail.com>
2026-05-23 13:20:41 -0300

    Add new grammar rules

--- parser/nodes.go
+++ parser/nodes.go
@@ -1,94 +1,162 @@
 package parser
 
+/*
+Expression:
+
+	Equality        == !=
+	Comparison      < > <= >=
+	Additive        + -
+	Multiplicative  * /
+	Unary           ! -
+	Postfix         function calls
+	Primary         literals, variables, (...)
+*/
 type (
-	//TODO BinaryExpression
-
-	//TODO Figure out how to represent parenthesized expressions without
-	//
-	// Could probably define ParenthesizedExpr as "(" @@ ")"
-	// and add it to the enum
-
-	//TODO IfStmt
-
-	//TODO WhileLoop
-
-	CompilationUnit struct {
-		// FIXME use an union to have different types of declarations
-		FuncDeclarations []FuncDeclaration `@@+`
+	Expr struct {
+		Equality *Equality `@@`
 	}
 
-	FuncDeclaration struct {
-		Func       string      `"func"`
-		Name       string      `@Ident`
-		LParen     string      `"("`
-		FuncParams []FuncParam `@@! ("," @@)*`
-		RParen     string      `")"`
-		ReturnType string      `@Ident`
-		LBrace     string      `"{"`
-		Stmts      []Stmt      `@@+`
-		RBrace     string      `"}"`
+	Equality struct {
+		Left  *Additive  `@@`
+		Right []*EqOpRhs `@@*`
 	}
 
-	FuncParam struct {
-		Ident string `@Ident`
-		Type  string `@Ident`
+	EqOpRhs struct {
+		Op    string    `@( "==" | "!=" )`
+		Right *Additive `@@`
 	}
 
-	ReturnStmt struct {
-		Return string `"return"`
-		Value  Expr   `@@`
+	Additive struct {
+		Left  *Multiplicative `@@`
+		Right []*AddOpRhs     `@@*`
 	}
 
-	VariableDeclarationStmt struct {
-		Var   string `"var"`
-		Ident string `@Ident`
-		Type  string `@Ident`
-		Eq    string `"="`
-		Expr  Expr   `@@`
+	AddOpRhs struct {
+		Op    string          `@( "+" | "-" )`
+		Right *Multiplicative `@@`
 	}
 
-	AssignmentStmt struct {
-		Ident string `@Ident`
-		Eq    string `"="`
-		Value Expr   `@@`
+	Multiplicative struct {
+		Left  *Unary      `@@`
+		Right []*MulOpRhs `@@*`
 	}
 
-	CallExpr struct {
-		Ident  string `@Ident`
-		LParen string `"("`
-		Params []Expr `@@! ("," @@)*`
-		RParen string `")"`
+	MulOpRhs struct {
+		Op    string `@( "*" | "/" )`
+		Right *Unary `@@`
 	}
 
-	IntegerExpr struct {
-		Value int64 `@Int`
+	Unary struct {
+		Op      *string  `@( "!" | "-" )?`
+		Operand *Postfix `@@`
 	}
 
-	StringExpr struct {
-		Value string `@String`
+	Postfix struct {
+		Primary *Primary `@@`
+		Calls   []*Call  `@@*`
 	}
 
-	UnaryExpr struct {
-		Operator string `"!"`
-		Operand  Expr   `@@`
+	Call struct {
+		Args []*Expr `"(" ( @@ ( "," @@ )* )? ")"`
 	}
 
-	VarExpr struct {
-		Ident string `@Ident`
+	Primary struct {
+		Number  *int    `  @Int`
+		String  *string `| @String`
+		Ident   *string `| @Ident`
+		Subexpr *Expr   `| "(" @@ ")"`
 	}
 )
 
-type (
-	Expr interface{ exprTag() }
-	Stmt interface{ stmtTag() }
-)
+//type (
+//TODO BinaryExpression
 
-func (expr CallExpr) exprTag()    {}
-func (expr IntegerExpr) exprTag() {}
-func (expr StringExpr) exprTag()  {}
-func (expr UnaryExpr) exprTag()   {}
-func (expr VarExpr) exprTag()     {}
+//TODO Figure out how to represent parenthesized expressions without
+//
+// Could probably define ParenthesizedExpr as "(" @@ ")"
+// and add it to the enum
 
-func (stmt AssignmentStmt) stmtTag()          {}
-func (stmt ReturnStmt) stmtTag()              {}
-func (stmt VariableDeclarationStmt) stmtTag() {}
+//TODO IfStmt
+
+//TODO WhileLoop
+
+//CompilationUnit struct {
+//	// FIXME use an union to have different types of declarations
+//	FuncDeclarations []FuncDeclaration `@@+`
+//}
+
+//FuncDeclaration struct {
+//	Func       string      `"func"`
+//	Name       string      `@Ident`
+//	LParen     string      `"("`
+//	FuncParams []FuncParam `@@! ("," @@)*`
+//	RParen     string      `")"`
+//	ReturnType string      `@Ident`
+//	LBrace     string      `"{"`
+//	Stmts      []Stmt      `@@+`
+//	RBrace     string      `"}"`
+//}
+
+//FuncParam struct {
+//	Ident string `@Ident`
+//	Type  string `@Ident`
+//}
+
+//ReturnStmt struct {
+//	Return string `"return"`
+//	Value  Expr   `@@`
+//}
+
+//VariableDeclarationStmt struct {
+//	Var   string `"var"`
+//	Ident string `@Ident`
+//	Type  string `@Ident`
+//	Eq    string `"="`
+//	Expr  Expr   `@@`
+//}
+
+//AssignmentStmt struct {
+//	Ident string `@Ident`
+//	Eq    string `"="`
+//	Value Expr   `@@`
+//}
+
+//CallExpr struct {
+//	Ident  string `@Ident`
+//	LParen string `"("`
+//	Params []Expr `@@! ("," @@)*`
+//	RParen string `")"`
+//}
+
+//IntegerExpr struct {
+//	Value int64 `@Int`
+//}
+
+//StringExpr struct {
+//	Value string `@String`
+//}
+
+//UnaryExpr struct {
+//	Operator string `"!"`
+//	Operand  Expr   `@@`
+//}
+
+//VarExpr struct {
+//	Ident string `@Ident`
+//}
+//)
+
+//type (
+//	Expr interface{ exprTag() }
+//	Stmt interface{ stmtTag() }
+//)
+//
+//func (expr CallExpr) exprTag()    {}
+//func (expr IntegerExpr) exprTag() {}
+//func (expr StringExpr) exprTag()  {}
+//func (expr UnaryExpr) exprTag()   {}
+//func (expr VarExpr) exprTag()     {}
+//
+//func (stmt AssignmentStmt) stmtTag()          {}
+//func (stmt ReturnStmt) stmtTag()              {}
+//func (stmt VariableDeclarationStmt) stmtTag() {}
--- parser/nodes_test.go
+++ parser/nodes_test.go
@@ -28,13 +28,13 @@ func testAll[T any](t *testing.T, testCases []TestCase[T]) {
 	}
 }
 
-// mustParse is a helper that parses a grammar element of type T from source code
+// // mustParse is a helper that parses a grammar element of type T from source code
 func mustParse[T any](t *testing.T, sourceCode string) *T {
 
 	parser, err := participle.Build[T](
 		participle.Unquote("String"), //FIXME figure out what this does
-		participle.Union[Stmt](AssignmentStmt{}, ReturnStmt{}, VariableDeclarationStmt{}),
-		participle.Union[Expr](CallExpr{}, IntegerExpr{}, StringExpr{}, UnaryExpr{}, VarExpr{}),
+		//participle.Union[Stmt](AssignmentStmt{}, ReturnStmt{}, VariableDeclarationStmt{}),
+		//participle.Union[Expr](CallExpr{}, IntegerExpr{}, StringExpr{}, UnaryExpr{}, VarExpr{}),
 	)
 	assert.NoError(t, err)
 
@@ -44,127 +44,305 @@ func mustParse[T any](t *testing.T, sourceCode string) *T {
 	return result
 }
 
-func Test_VariableDeclarationStmt(t *testing.T) {
+//func Test_VariableDeclarationStmt(t *testing.T) {
+//
+//	testCases := []TestCase[VariableDeclarationStmt]{
+//		{
+//			Name:       "naive case",
+//			SourceCode: "var a int = 1",
+//			Expected: VariableDeclarationStmt{
+//				Ident: "a",
+//				Type:  "int",
+//				Expr:  IntegerExpr{Value: 1},
+//			},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
+//func Test_Stmt(t *testing.T) {
+//
+//	testCases := []TestCase[Stmt]{
+//		{
+//			Name:       `assignment stmt`,
+//			SourceCode: `a = 1`,
+//			Expected: AssignmentStmt{
+//				Ident: "a",
+//				Value: IntegerExpr{Value: 1},
+//			},
+//		},
+//		{
+//			Name:       `return stmt`,
+//			SourceCode: `return 1`,
+//			Expected:   ReturnStmt{Value: IntegerExpr{Value: 1}},
+//		},
+//		{
+//			Name:       `variable declaration stmt`,
+//			SourceCode: `var s string = "foo"`,
+//			Expected: VariableDeclarationStmt{
+//				Ident: "s",
+//				Type:  "string",
+//				Expr:  StringExpr{Value: "foo"},
+//			},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
+//func Test_AssignmentStmt(t *testing.T) {
+//
+//	testCases := []TestCase[AssignmentStmt]{
+//		{
+//			Name:       `assignment stmt`,
+//			SourceCode: `a = 1`,
+//			Expected: AssignmentStmt{
+//				Ident: "a",
+//				Value: IntegerExpr{Value: 1},
+//			},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
+//func Test_ReturnStmt(t *testing.T) {
+//
+//	testCases := []TestCase[ReturnStmt]{
+//		{
+//			Name:       `just return 0`,
+//			SourceCode: `return 0`,
+//			Expected:   ReturnStmt{Value: IntegerExpr{Value: 0}},
+//		},
+//		{
+//			Name:       `just return 1`,
+//			SourceCode: `return 1`,
+//			Expected:   ReturnStmt{Value: IntegerExpr{Value: 1}},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
+//func Test_FuncParam(t *testing.T) {
+//
+//	testCases := []TestCase[FuncParam]{
+//		{
+//			Name:       "Naive case",
+//			SourceCode: "j int",
+//			Expected: FuncParam{
+//				Ident: "j",
+//				Type:  "int",
+//			},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
+//func Test_FuncDeclaration(t *testing.T) {
+//
+//	testCases := []TestCase[FuncDeclaration]{
+//		{
+//			Name: "Naive case",
+//			SourceCode: `
+//				func main(i int) int {
+//					return 0
+//				}
+//			`,
+//			Expected: FuncDeclaration{
+//				Name: "main",
+//				FuncParams: []FuncParam{
+//					{
+//						Ident: "i",
+//						Type:  "int",
+//					},
+//				},
+//				ReturnType: "int",
+//				Stmts: []Stmt{
+//					ReturnStmt{
+//						Value: IntegerExpr{Value: 0},
+//					},
+//				},
+//			},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
+//func Test_CallExpr(t *testing.T) {
+//
+//	testCases := []TestCase[CallExpr]{
+//		{
+//			Name:       "call expression with one param",
+//			SourceCode: `strlen("foo")`,
+//			Expected: CallExpr{
+//				Ident: "strlen",
+//				Params: []Expr{
+//					StringExpr{Value: "foo"},
+//				},
+//			},
+//		},
+//		{
+//			Name:       "call expression with two params",
+//			SourceCode: `printf("number: %d", 1)`,
+//			Expected: CallExpr{
+//				Ident: "printf",
+//				Params: []Expr{
+//					StringExpr{Value: "number: %d"},
+//					IntegerExpr{Value: 1},
+//				},
+//			},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
+//func Test_IntegerExpr(t *testing.T) {
+//
+//	testCases := []TestCase[IntegerExpr]{
+//		{
+//			Name:       "int literal",
+//			SourceCode: `42`,
+//			Expected:   IntegerExpr{Value: 42},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//func Test_StringExpr(t *testing.T) {
+//
+//	testCases := []TestCase[StringExpr]{
+//		{
+//			Name:       "string literal",
+//			SourceCode: `"this is a string literal"`,
+//			Expected:   StringExpr{Value: "this is a string literal"},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
+//func Test_UnaryExpr(t *testing.T) {
+//
+//	testCases := []TestCase[UnaryExpr]{
+//		{
+//			Name:       "unary expression with variable expression",
+//			SourceCode: `!a`,
+//			Expected: UnaryExpr{
+//				Operand: VarExpr{
+//					Ident: "a",
+//				},
+//			},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
+//func Test_VarExpr(t *testing.T) {
+//
+//	testCases := []TestCase[VarExpr]{
+//		{
+//			Name:       "variable expression",
+//			SourceCode: `a`,
+//			Expected:   VarExpr{Ident: "a"},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
+//func Test_Expr(t *testing.T) {
+//
+//	testCases := []TestCase[Expr]{
+//		{
+//			Name:       "function call expression",
+//			SourceCode: `printf("number: %d", 1)`,
+//			Expected: CallExpr{
+//				Ident: "printf",
+//				Params: []Expr{
+//					StringExpr{Value: "number: %d"},
+//					IntegerExpr{Value: 1},
+//				},
+//			},
+//		},
+//		{
+//			Name:       "int literal",
+//			SourceCode: `42`,
+//			Expected:   IntegerExpr{Value: 42},
+//		},
+//		{
+//			Name:       "string literal",
+//			SourceCode: `"this is a string literal"`,
+//			Expected:   StringExpr{Value: "this is a string literal"},
+//		},
+//		{
+//			Name:       "unary expression with variable expression",
+//			SourceCode: `!a`,
+//			Expected: UnaryExpr{
+//				Operand: VarExpr{
+//					Ident: "a",
+//				},
+//			},
+//		},
+//		{
+//			Name:       "variable expression",
+//			SourceCode: `a`,
+//			Expected:   VarExpr{Ident: "a"},
+//		},
+//	}
+//
+//	testAll(t, testCases)
+//}
+//
 
-	testCases := []TestCase[VariableDeclarationStmt]{
+func Test_Primary(t *testing.T) {
+
+	testCases := []TestCase[Primary]{
 		{
-			Name:       "naive case",
-			SourceCode: "var a int = 1",
-			Expected: VariableDeclarationStmt{
-				Ident: "a",
-				Type:  "int",
-				Expr:  IntegerExpr{Value: 1},
+			Name:       "number",
+			SourceCode: `1`,
+			Expected: Primary{
+				Number: ptr(1),
 			},
 		},
-	}
-
-	testAll(t, testCases)
-}
-
-func Test_Stmt(t *testing.T) {
-
-	testCases := []TestCase[Stmt]{
 		{
-			Name:       `assignment stmt`,
-			SourceCode: `a = 1`,
-			Expected: AssignmentStmt{
-				Ident: "a",
-				Value: IntegerExpr{Value: 1},
+			Name:       "string",
+			SourceCode: `"s"`,
+			Expected: Primary{
+				String: ptr("s"),
 			},
 		},
 		{
-			Name:       `return stmt`,
-			SourceCode: `return 1`,
-			Expected:   ReturnStmt{Value: IntegerExpr{Value: 1}},
-		},
-		{
-			Name:       `variable declaration stmt`,
-			SourceCode: `var s string = "foo"`,
-			Expected: VariableDeclarationStmt{
-				Ident: "s",
-				Type:  "string",
-				Expr:  StringExpr{Value: "foo"},
+			Name:       "ident",
+			SourceCode: `a`,
+			Expected: Primary{
+				Ident: ptr("a"),
 			},
 		},
-	}
-
-	testAll(t, testCases)
-}
-
-func Test_AssignmentStmt(t *testing.T) {
-
-	testCases := []TestCase[AssignmentStmt]{
 		{
-			Name:       `assignment stmt`,
-			SourceCode: `a = 1`,
-			Expected: AssignmentStmt{
-				Ident: "a",
-				Value: IntegerExpr{Value: 1},
-			},
-		},
-	}
-
-	testAll(t, testCases)
-}
-
-func Test_ReturnStmt(t *testing.T) {
-
-	testCases := []TestCase[ReturnStmt]{
-		{
-			Name:       `just return 0`,
-			SourceCode: `return 0`,
-			Expected:   ReturnStmt{Value: IntegerExpr{Value: 0}},
-		},
-		{
-			Name:       `just return 1`,
-			SourceCode: `return 1`,
-			Expected:   ReturnStmt{Value: IntegerExpr{Value: 1}},
-		},
-	}
-
-	testAll(t, testCases)
-}
-
-func Test_FuncParam(t *testing.T) {
-
-	testCases := []TestCase[FuncParam]{
-		{
-			Name:       "Naive case",
-			SourceCode: "j int",
-			Expected: FuncParam{
-				Ident: "j",
-				Type:  "int",
-			},
-		},
-	}
-
-	testAll(t, testCases)
-}
-
-func Test_FuncDeclaration(t *testing.T) {
-
-	testCases := []TestCase[FuncDeclaration]{
-		{
-			Name: "Naive case",
-			SourceCode: `
-				func main(i int) int {
-					return 0
-				}
-			`,
-			Expected: FuncDeclaration{
-				Name: "main",
-				FuncParams: []FuncParam{
-					{
-						Ident: "i",
-						Type:  "int",
+			Name:       "subexpr",
+			SourceCode: `(1)`,
+			Expected: Primary{
+				Subexpr: &Expr{
+					Equality: &Equality{
+						Left: &Additive{
+							Left: &Multiplicative{
+								Left: &Unary{
+									Operand: &Postfix{
+										Primary: &Primary{
+											Number: ptr(1),
+										},
+									},
+								},
+							},
+						},
 					},
 				},
-				ReturnType: "int",
-				Stmts: []Stmt{
-					ReturnStmt{
-						Value: IntegerExpr{Value: 0},
-					},
-				},
 			},
 		},
 	}
@@ -172,129 +350,6 @@ func Test_FuncDeclaration(t *testing.T) {
 	testAll(t, testCases)
 }
 
-func Test_CallExpr(t *testing.T) {
-
-	testCases := []TestCase[CallExpr]{
-		{
-			Name:       "call expression with one param",
-			SourceCode: `strlen("foo")`,
-			Expected: CallExpr{
-				Ident: "strlen",
-				Params: []Expr{
-					StringExpr{Value: "foo"},
-				},
-			},
-		},
-		{
-			Name:       "call expression with two params",
-			SourceCode: `printf("number: %d", 1)`,
-			Expected: CallExpr{
-				Ident: "printf",
-				Params: []Expr{
-					StringExpr{Value: "number: %d"},
-					IntegerExpr{Value: 1},
-				},
-			},
-		},
-	}
-
-	testAll(t, testCases)
-}
-
-func Test_IntegerExpr(t *testing.T) {
-
-	testCases := []TestCase[IntegerExpr]{
-		{
-			Name:       "int literal",
-			SourceCode: `42`,
-			Expected:   IntegerExpr{Value: 42},
-		},
-	}
-
-	testAll(t, testCases)
-}
-func Test_StringExpr(t *testing.T) {
-
-	testCases := []TestCase[StringExpr]{
-		{
-			Name:       "string literal",
-			SourceCode: `"this is a string literal"`,
-			Expected:   StringExpr{Value: "this is a string literal"},
-		},
-	}
-
-	testAll(t, testCases)
-}
-
-func Test_UnaryExpr(t *testing.T) {
-
-	testCases := []TestCase[UnaryExpr]{
-		{
-			Name:       "unary expression with variable expression",
-			SourceCode: `!a`,
-			Expected: UnaryExpr{
-				Operand: VarExpr{
-					Ident: "a",
-				},
-			},
-		},
-	}
-
-	testAll(t, testCases)
-}
-
-func Test_VarExpr(t *testing.T) {
-
-	testCases := []TestCase[VarExpr]{
-		{
-			Name:       "variable expression",
-			SourceCode: `a`,
-			Expected:   VarExpr{Ident: "a"},
-		},
-	}
-
-	testAll(t, testCases)
-}
-
-func Test_Expr(t *testing.T) {
-
-	testCases := []TestCase[Expr]{
-		{
-			Name:       "function call expression",
-			SourceCode: `printf("number: %d", 1)`,
-			Expected: CallExpr{
-				Ident: "printf",
-				Params: []Expr{
-					StringExpr{Value: "number: %d"},
-					IntegerExpr{Value: 1},
-				},
-			},
-		},
-		{
-			Name:       "int literal",
-			SourceCode: `42`,
-			Expected:   IntegerExpr{Value: 42},
-		},
-		{
-			Name:       "string literal",
-			SourceCode: `"this is a string literal"`,
-			Expected:   StringExpr{Value: "this is a string literal"},
-		},
-		{
-			Name:       "unary expression with variable expression",
-			SourceCode: `!a`,
-			Expected: UnaryExpr{
-				Operand: VarExpr{
-					Ident: "a",
-				},
-			},
-		},
-		{
-			Name:       "variable expression",
-			SourceCode: `a`,
-			Expected:   VarExpr{Ident: "a"},
-		},
-	}
-
-	testAll(t, testCases)
+func ptr[T any](t T) *T {
+	return &t
 }
