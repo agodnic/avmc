b77eb0648a7694801ecaace0373a57190119a3a8
Agustin Godnic <agustingodnic@gmail.com>
2026-05-30 01:04:33 -0300

    Add specific test for stmts

--- parser/helpers_test.go
+++ parser/helpers_test.go
@@ -1,8 +1,10 @@
 package parser
 
 import (
+	"fmt"
 	"testing"
 
+	"github.com/agodnic/avmc/parser/cst"
 	"github.com/agodnic/avmc/parser/generated/lexer"
 	"github.com/agodnic/avmc/parser/generated/parser"
 	"github.com/stretchr/testify/assert"
@@ -36,7 +38,7 @@ func testAll(t *testing.T, tcs []TestCase) {
 	}
 }
 
-///////
+///////////////////////////////
 
 type TestCaseInsideFunctionBody struct {
 	Name   string
@@ -44,24 +46,29 @@ type TestCaseInsideFunctionBody struct {
 	Output any
 }
 
-func parseFunctionBody(sourceCode []byte) (any, error) {
-
-	lex := lexer.NewLexer([]byte(sourceCode))
-	p := parser.NewParser()
-
-	return p.Parse(lex)
-}
-
 func testAllInsideFunctionBody(t *testing.T, tcs []TestCaseInsideFunctionBody) {
 	for _, tc := range tcs {
 		t.Run(tc.Name, func(t *testing.T) {
 
-			tree, err := parseFunctionBody([]byte(tc.Input))
+			const functionName = "functionNameThatShouldNowShadowAnything"
+			src := fmt.Sprintf(`func %s () { %s };`, functionName, tc.Input)
+
+			actualTree, err := parse([]byte(src))
 			if !assert.NoError(t, err) {
 				return
 			}
 
-			assert.Equal(t, tc.Output, tree)
+			expectedTree := cst.FuncDecl{
+				Ident: functionName,
+				Type:  cst.VoidType{},
+				Block: cst.Block{
+					Stmts: []any{
+						tc.Output,
+					},
+				},
+			}
+
+			assert.Equal(t, expectedTree, actualTree)
 		})
 	}
 }
