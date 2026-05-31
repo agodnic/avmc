2749153724d187beed706e4d9cd3a70773eb6908
Agustin Godnic <agustingodnic@gmail.com>
2026-05-31 20:43:54 -0300

    Grammar: change binary op rules

--- parser/precedence_test.go
+++ parser/precedence_test.go
@@ -6,58 +6,25 @@ import (
 	"github.com/agodnic/avmc/parser/cst"
 )
 
-func Test_OperatorPrecedence(t *testing.T) {
+func Test_ParenthesizedExpr(t *testing.T) {
 
 	tcs := []TestForStmt{
 		{
-			Name:  "+ has lower precedence than *",
-			Input: `1 + 2 * 3;`,
+			Name:  "parenthesized uint literal",
+			Input: `(1);`,
+			Output: cst.ParenthesizedExpr{
+				Expr: cst.UintLit{Value: 1},
+			},
+		},
+		{
+			Name:  "parenthesized operand in binary operation",
+			Input: `1 + (2);`,
 			Output: cst.BinOp{
 				R:  cst.UintLit{Value: 1},
 				Op: "+",
-				L: cst.BinOp{
-					R:  cst.UintLit{Value: 2},
-					Op: "*",
-					L:  cst.UintLit{Value: 3},
+				L: cst.ParenthesizedExpr{
+					Expr: cst.UintLit{Value: 2},
 				},
-			},
-		},
-		{
-			Name:  "* has higher precedence than +",
-			Input: `1 * 2 + 3;`,
-			Output: cst.BinOp{
-				R: cst.BinOp{
-					R:  cst.UintLit{Value: 1},
-					Op: "*",
-					L:  cst.UintLit{Value: 2},
-				},
-				Op: "+",
-				L:  cst.UintLit{Value: 3},
-			},
-		},
-		{
-			Name:  "parenthesized expressions have higher precedence than *",
-			Input: `1 * (2 + 3);`,
-			Output: cst.BinOp{
-				R:  cst.UintLit{Value: 1},
-				Op: "*",
-				L: cst.BinOp{
-					R:  cst.UintLit{Value: 2},
-					Op: "+",
-					L:  cst.UintLit{Value: 3},
-				},
-			},
-		},
-		{
-			Name:  "unary - has higher precedence than +",
-			Input: `-1 + 2;`,
-			Output: cst.BinOp{
-				R: cst.UnaryOp{
-					Op:   "-",
-					Expr: cst.UintLit{Value: 1},
-				},
-				Op: "+",
-				L:  cst.UintLit{Value: 2},
 			},
 		},
 	}
