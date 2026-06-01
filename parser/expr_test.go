bd1909bff235a123f189be5899e423c1b61ce75f
Agustin Godnic <agustingodnic@gmail.com>
2026-05-31 21:01:57 -0300

    Refactor

--- parser/expr_test.go
+++ parser/expr_test.go
@@ -46,6 +46,31 @@ func Test_BinOp(t *testing.T) {
 
 	testStmts(t, tcs)
 }
+func Test_ParenthesizedExpr(t *testing.T) {
+
+	tcs := []TestForStmt{
+		{
+			Name:  "parenthesized uint literal",
+			Input: `(1);`,
+			Output: cst.ParenthesizedExpr{
+				Expr: cst.UintLit{Value: 1},
+			},
+		},
+		{
+			Name:  "parenthesized operand in binary operation",
+			Input: `1 + (2);`,
+			Output: cst.BinOp{
+				R:  cst.UintLit{Value: 1},
+				Op: "+",
+				L: cst.ParenthesizedExpr{
+					Expr: cst.UintLit{Value: 2},
+				},
+			},
+		},
+	}
+
+	testStmts(t, tcs)
+}
 
 func Test_IndexExpr(t *testing.T) {
 
